cb7d0d45f9641332603716226b0d831b988fad9a
KaushalPranaw <pranaw.kaushal.1997@gmail.com>
2020-02-27 17:05:52 +0530

    emp department job jobhistory pojo committed

--- EmployeeDB/src/com/yash/entities/Employee.java
+++ EmployeeDB/src/com/yash/entities/Employee.java
@@ -1,5 +1,140 @@
 package com.yash.entities;
 
+import java.util.Date;
+
+/*
+ * 
+ * Created By Pranaw
+ */
 public class Employee {
+
+	private int employeeId;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String phoneNumber;
+	private Date hireDate;
+	private double salary;
+	private double commissionPct;
+	private String jobId;
+	private int managerId;
+	private int departmentId;
+
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Employee(int employeeId, String firstName, String lastName, String email, String phoneNumber, Date hireDate,
+			double salary, double commissionPct, String jobId, int managerId, int departmentId) {
+		super();
+		this.employeeId = employeeId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+		this.hireDate = hireDate;
+		this.salary = salary;
+		this.commissionPct = commissionPct;
+		this.jobId = jobId;
+		this.managerId = managerId;
+		this.departmentId = departmentId;
+	}
+
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public Date getHireDate() {
+		return hireDate;
+	}
+
+	public void setHireDate(Date hireDate) {
+		this.hireDate = hireDate;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public double getCommissionPct() {
+		return commissionPct;
+	}
+
+	public void setCommissionPct(double commissionPct) {
+		this.commissionPct = commissionPct;
+	}
+
+	public String getJobId() {
+		return jobId;
+	}
+
+	public void setJobId(String jobId) {
+		this.jobId = jobId;
+	}
+
+	public int getManagerId() {
+		return managerId;
+	}
+
+	public void setManagerId(int managerId) {
+		this.managerId = managerId;
+	}
+
+	public int getDepartmentId() {
+		return departmentId;
+	}
+
+	public void setDepartmentId(int departmentId) {
+		this.departmentId = departmentId;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [employeeId=" + employeeId + ", firstName=" + firstName + ", lastName=" + lastName + ", email="
+				+ email + ", phoneNumber=" + phoneNumber + ", hireDate=" + hireDate + ", salary=" + salary
+				+ ", commissionPct=" + commissionPct + ", jobId=" + jobId + ", managerId=" + managerId
+				+ ", departmentId=" + departmentId + "]";
+	}
 
 }
