cbd89939568f70769a93ab37715d71ac4bb16a92
siddhant.vora <siddhant.vora@YP1009401DT.Yash.com>
2020-02-27 17:41:14 +0530

    Library added

--- EmployeeDB/src/com/yash/tester/EmployeeTest.java
+++ EmployeeDB/src/com/yash/tester/EmployeeTest.java
@@ -1,5 +1,15 @@
 package com.yash.tester;
 
 public class EmployeeTest {
+	
+	public static void main(String args[]) {
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 }
