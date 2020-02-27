b18255f8b865a1547a9b51b3fc054bcf87c2fe29
siddhant.vora <siddhant.vora@YP1009401DT.Yash.com>
2020-02-27 17:07:30 +0530

    location-country-region pojo commit

--- EmployeeDB/src/com/yash/entities/Country.java
+++ EmployeeDB/src/com/yash/entities/Country.java
@@ -1,5 +1,41 @@
 package com.yash.entities;
 
 public class Country {
-
+	private String countryId;
+	private String CountryName;
+	private Region region;
+	
+	public Country() {
+		
+	}
+	public Country(String countryId, String countryName, Region region) {
+	
+		this.countryId = countryId;
+		CountryName = countryName;
+		this.region = region;
+	}
+	public String getCountryId() {
+		return countryId;
+	}
+	public void setCountryId(String countryId) {
+		this.countryId = countryId;
+	}
+	public String getCountryName() {
+		return CountryName;
+	}
+	public void setCountryName(String countryName) {
+		CountryName = countryName;
+	}
+	public Region getRegion() {
+		return region;
+	}
+	public void setRegion(Region region) {
+		this.region = region;
+	}
+	@Override
+	public String toString() {
+		return "Country [countryId=" + countryId + ", CountryName=" + CountryName + ", region=" + region + "]";
+	}
+	
+	
 }
--- EmployeeDB/src/com/yash/entities/Region.java
+++ EmployeeDB/src/com/yash/entities/Region.java
@@ -1,5 +1,33 @@
 package com.yash.entities;
 
 public class Region {
-
+	private int regionId;
+	private String regionName;
+	
+	public Region() {
+		
+	}
+	public Region(int regionId, String regionName) {
+		
+		this.regionId = regionId;
+		this.regionName = regionName;
+	}
+	public int getRegionId() {
+		return regionId;
+	}
+	public void setRegionId(int regionId) {
+		this.regionId = regionId;
+	}
+	public String getRegionName() {
+		return regionName;
+	}
+	public void setRegionName(String regionName) {
+		this.regionName = regionName;
+	}
+	
+	@Override
+	public String toString() {
+		return "Region [regionId=" + regionId + ", regionName=" + regionName + "]";
+	}
+	
 }
